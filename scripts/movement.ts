78c7a0de141a6c89016f0672d385bb55fade2faa
job-gut <104257231+job-gut@users.noreply.github.com>
2023-02-03 23:13:51 +0900

    add functions

--- scripts/movement.ts
+++ scripts/movement.ts
@@ -1,26 +1,47 @@
+import { BlockPos } from "bdsx/bds/blockpos";
 import { MinecraftPacketIds } from "bdsx/bds/packetids";
 import { Player } from "bdsx/bds/player";
 import { events } from "bdsx/event";
 
+const lastBPS: Record<string, number> = {};
+const isSpinAttacking: Record<string, boolean> = {};
+
 const lastpos: Record<string, number[]> = {};
 const strafestack: Record<string, number> = {};
-const lastbps: Record<string, number> = {};
 const getDamaged: Record<string, boolean> = {};
-const isSpinAttacking: Record<string, boolean> = {};
 const isTeleported: Record<string, boolean> = {};
 const haveFished: Record<string, boolean> = {};
-const onIce: Record<string, boolean> = {};
 
 declare module "bdsx/bds/player" {
     interface Player {
+        /**
+         * Returns if player is on ices
+         */
         onIce(): boolean;
+        //No JSDoc
+        isSpinAttacking(): boolean;
+        /**
+         * Returns player's Last Blocks per second
+         * @deprecated Just define.
+         */
+        lastBPS(): number;
     }
-}
+};
+
 Player.prototype.onIce = function () {
-    // const
-    // this.getRegion().getBlock(this.get())
-    return true;
-}
+    const pos = BlockPos.create(this.getFeetPos());
+    pos.y--;
+
+    const blockName = this.getRegion().getBlock(pos).getName();
+    if (blockName.includes("ice")) return true
+    else return false;
+};
+
+Player.prototype.isSpinAttacking = function () {
+    const plname = this.getNameTag();
+    if (!isSpinAttacking[plname]) isSpinAttacking[plname] = false;
+    return isSpinAttacking[plname];
+};
 
 events.packetBefore(MinecraftPacketIds.MovePlayer).on((pkt, ni) => {
 
