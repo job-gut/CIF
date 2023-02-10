360fb5841dac4451b6590ffbf179802a1b0ed88f
job-gut <104257231+job-gut@users.noreply.github.com>
2023-02-10 18:34:02 +0900

    함수 싹 정리

--- main.ts
+++ main.ts
@@ -6,82 +6,40 @@ if (serverProperties["server-authoritative-movement"] !== "client-auth") {
 
 import { NetworkIdentifier } from "bdsx/bds/networkidentifier";
 import { CANCEL } from "bdsx/common";
-import { bedrockServer } from "bdsx/launcher";
 import { CommandPermissionLevel } from "bdsx/bds/command";
 
-import { nameMap, deviceModelMap } from "./scripts/join";
+import { announce_CIF, ban_CIF, detect_CIF, ipDetect_CIF, log_CIF } from "./functions";
 
 import "./scripts";
 
-/**
- * @deprecated 다른 곳에서 임의로 쓰지 마세요
- */
-function zero(num: any, n: any) {
-    let zero = "";
-    let num2 = num.toString();
-    if (num2.length < n) {
-        for (var i = 0; i < n - num2.length; i++)
-            zero += "0";
-    }
-    return zero + num;
-};
-
-
-/**
- * Date With Zero
- */
-function dateWithZero() {
-    var d = new Date();
-    return (d.getFullYear() + "-" + zero((d.getMonth() + 1), 2) + "-"
-        + zero(d.getDate(), 2) + ", " + zero(d.getHours(), 2) + "시 "
-        + zero(d.getMinutes(), 2) + "분 " + zero(d.getSeconds(), 2) + "초 " + zero(d.getMilliseconds(), 3));
-}
 
 export namespace CIF {
+
     /**
      * Send messages to players
      * @param message 보낼 메세지
      * @param target Permission
      */
-    export function announce(message: string, target: CommandPermissionLevel | "ALL" = CommandPermissionLevel.Operator) {
-        let users;
-        if (target === "ALL") {
-            users = bedrockServer.serverInstance.getPlayers();
-        } else {
-            users = bedrockServer.serverInstance.getPlayers().filter(p => p.getCommandPermissionLevel() === target);
-        };
-
-        for (const member of users) {
-            if (wasDetected[member.getNameTag()] === true) continue;
-            member.sendMessage(message);
-        };
+    export function announce(message: string, target: CommandPermissionLevel | "ALL" = CommandPermissionLevel.Operator): void {
+        return announce_CIF(message, target);
     };
+
 
     /**
     * 콘솔에 로그를 남깁니다
     * @param message 콘솔에 남길 문자
     */
     export function log(message: string): void {
-        const date = new Date();
-        console.info(
-            "[" + date.getFullYear() + "-" + zero((date.getMonth() + 1), 2) + "-" + zero(date.getDate(), 2) +
-            " " + zero(date.getHours(), 2) + ":" + zero(date.getMinutes(), 2) + ":" + zero(date.getSeconds(), 2) + 
-            ":" + zero(date.getMilliseconds(), 3) + " INFO] " + "[CIF] ".red + message);
+        return log_CIF(message);
     };
 
 
     /**
      * 대충 밴 함수
-     * @description 현재 밴 기능 수행 X
+     * @deprecated just define
      */
-    export function ban(
-        ni: NetworkIdentifier,
-        reason: string
-    ): void {
-        const cheaterName = nameMap.get(ni)!;
-        wasDetected[cheaterName] = true;
-        announce(`§c[§fCIF§c] §c${cheaterName} §6was banned using §c${reason}`, "ALL");
-        log(`${cheaterName} was banned using ${reason}`);
+    export function ban(ni: NetworkIdentifier, reason: string): void {
+        return ban_CIF(ni, reason);
     };
 
 
@@ -89,29 +47,19 @@ export namespace CIF {
      * 대충 핵 감지됐을 때 쓰는 함수
      * @description CIF.ban() 은 이 함수에서 호출 안 함
      */
-    export function detect(
-        ni: NetworkIdentifier,
-        cheatName: string,
-        cheatDescription: string
-    ): CANCEL {
-        const cheaterName = nameMap.get(ni)!;
-        wasDetected[cheaterName] = true;
-        // bedrockServer.serverInstance.disconnectClient(ni, `§l§f[§cCIF§f]\n§b${cheatName} Detected`);
-        announce(`§c[§fCIF§c] §c${cheaterName} §6was blocked all-packets using §c${cheatName} §7(${cheatDescription})`);
-        log(`${cheaterName} was blocked all-packets using ${cheatName} (${cheatDescription})`);
-        return CANCEL;
+    export function detect(ni: NetworkIdentifier, cheatName: string, cheatDescription: string): CANCEL {
+        return detect_CIF(ni, cheatName, cheatDescription);
     };
+
 
     /**
      * CVE 감지 때 쓰는 함수
      * @description CIF.ban() 은 이 함수에서 호출 안 함
      */
-    export function ipDetect(ni: NetworkIdentifier, cheatName: string, cheatDescription: string) {
-        const ip = ni.getAddress().split("|")[0];
-        announce(`§c[§fCIF§c] §c${ip} §6was ip-blocked using §c${cheatName} §7(${cheatDescription})`);
-        log(`${ip} was ip-blocked using ${cheatName} (${cheatDescription})`);
-        return CANCEL;
+    export function ipDetect(ni: NetworkIdentifier, cheatName: string, cheatDescription: string): void {
+        return ipDetect_CIF(ni, cheatName, cheatDescription);
     };
+
 
     export const wasDetected: Record<string, boolean> = {};
 };
