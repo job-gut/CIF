128480169a50a14e742de9b8e212af9d0a63c7e0
job-gut <104257231+job-gut@users.noreply.github.com>
2023-02-22 00:34:38 +0900

    dude do not upload debug codes

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import { serverProperties } from "bdsx/serverproperties";
 import { NetworkIdentifier } from "bdsx/bds/networkidentifier";
-import { CommandMessage, CommandPermissionLevel, PlayerCommandSelector } from "bdsx/bds/command";
+import { CommandPermissionLevel } from "bdsx/bds/command";
 import { CANCEL } from "bdsx/common";
 import "./scripts";
 
@@ -66,45 +66,3 @@ export namespace CIF {
 };
 
 import "./implements";
-import { events } from "bdsx/event";
-import { command } from "bdsx/command";
-import { ServerPlayer } from "bdsx/bds/player";
-
-//debug code
-events.serverOpen.on(() => {
-    const debugCommand = command.register('cif', '', CommandPermissionLevel.Operator);
-    debugCommand.overload((param, origin, output) => {
-        CIF.announce(param.message.getMessage(origin), param.target);
-    }, {
-        announce: command.enum("announce"),
-        message: CommandMessage,
-        target: [command.enum("target", CommandPermissionLevel), true]
-    });
-    debugCommand.overload((param, origin, output) => {
-        CIF.log(param.message.getMessage(origin));
-    }, {
-        log: command.enum("log"),
-        message: CommandMessage
-    });
-    debugCommand.overload((param, origin, output) => {
-        let player: any = param.target.newResults(origin, ServerPlayer);
-        if (player.length !== 1) return output.error("한명을 선택해주세요");
-        player = player[0];
-        CIF.ban(player.getNetworkIdentifier(), param.reason.getMessage(origin));
-    }, {
-        ban: command.enum("ban"),
-        target: PlayerCommandSelector,
-        reason: CommandMessage
-    });
-    debugCommand.overload((param, origin, output) => {
-        let player: any = param.target.newResults(origin, ServerPlayer);
-        if (player.length !== 1) return output.error("한명을 선택해주세요");
-        player = player[0];
-        CIF.detect(player.getNetworkIdentifier(), param.cheatName.getMessage(origin), param.cheatDescription.getMessage(origin));
-    }, {
-        detect: command.enum("detect"),
-        target: PlayerCommandSelector,
-        cheatName: CommandMessage,
-        cheatDescription: CommandMessage
-    });
-});
