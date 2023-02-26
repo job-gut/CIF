d6d133742c70cd4d9c0e60479f2ab6928d3a0564
wwwcomcomcomcom <iieiiergn@gmail.com>
2023-02-26 14:19:42 +0900

    reach fix - maybe

--- scripts/reach.ts
+++ scripts/reach.ts
@@ -5,28 +5,27 @@ import { CIF } from "../main";
 
 const reachWarn = new Map<NetworkIdentifier, number>();
 
-function warn(ni: NetworkIdentifier): CANCEL {
+function warn(ni: NetworkIdentifier, reach: number): CANCEL {
 
     if (!reachWarn.get(ni)) reachWarn.set(ni, 0);
 
-    reachWarn.set(ni, reachWarn.get(ni)! + 1);
+    reachWarn.set(ni, reachWarn.get(ni)! + reach);
 
     setTimeout(() => {
-        reachWarn.set(ni, reachWarn.get(ni)! - 1);
+        reachWarn.set(ni, reachWarn.get(ni)! - reach);
     }, 5000);
 
-    if (reachWarn.get(ni)! > 2) {
+    if (reachWarn.get(ni)! > 3) {
         CIF.detect(ni, "reach", "Increase Reach");
     };
 
     return CANCEL;
 };
 
-events.entityHurt.on((ev) => {
-    const victim = ev.entity;
-    const attacker = ev.damageSource.getDamagingEntity();
-    if (!attacker) return;
-    if (!attacker.isPlayer()) return;
+events.playerAttack.on((ev) => {
+    const victim = ev.victim;
+    const attacker = ev.player;
+    if (!attacker?.isPlayer() || !victim.isPlayer()) return;
     if (attacker.getGameType() === 1) return;
 
     const attackerpos = attacker.getFeetPos();
@@ -38,6 +37,6 @@ events.entityHurt.on((ev) => {
     const reach = Math.sqrt(result1 + result2);
     // console.log(reach); debug
     if (reach > 4.75) {
-        return warn(attacker.getNetworkIdentifier());
+        return warn(attacker.getNetworkIdentifier(), reach - 4.75);
     };
 });
