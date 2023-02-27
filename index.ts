89f1246406262026f012abc8234fb55a7ce9df51
job-gut <newdiscordalt6@gmail.com>
2023-02-27 18:22:39 +0900

    Revert "fix some bugs"
    This reverts commit ee3ddd9cd5421da22bf9ac18e6a23fc2bfe56461.

--- index.ts
+++ index.ts
@@ -1,14 +1,1 @@
-import "./bug";
-
-import "./log";
-
-//import "./give";
-//Mojang Patched Fake InventoryTransAction packets themselves
-
-import "./xp";
-import "./crasher";
-import "./join";
-import "./movement";
-import "./instabreak";
-import "./combat";
-import "./scaffold";
+import "./main";
