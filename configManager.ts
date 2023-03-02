c9a1ce07cd8abd065642b2a20da89e04550a4abf
wwwcomcomcomcom <iieiiergn@gmail.com>
2023-03-02 22:22:07 +0900

    conffffffffffffffig

--- configManager.ts
+++ configManager.ts
@@ -1,26 +1,88 @@
-import { existsSync } from "fs";
+import * as fs from "fs";
+import { CIF } from "./main";
 
-export namespace configManager {
-    export namespace configs {
-        export namespace modules {
-            
-        };
-    };
+export namespace CIFconfig {
+    export const Modules: ConfigType = {
+        bug: true,
+        combat: true,
+        crasher: true,
+        give: true,
+        instabreak: true,
+        join: true,
+        log: true,
+        movement: true,
+        scaffold: true,
+        xp: true
+    }
+    export const Penalties: ConfigType = {
+        ban: true,
+        kick: true,
+        send_to_member: true
+    }
+}
 
-    export function createNewFile(): void {
-        const options: any = {};
-        options.module = {};
-    };
 
+function createNewFile(): void {
+    fs.writeFileSync("../plugins/CIF/options.txt",
+        `bug = true
+    combat = true
+    crasher = true
+    give = true
+    instabreak = true
+    join = true
+    log = true
+    movement = true
+    scaffold = true
+    xp = true
+    ban = true
+    kick = true
+    send_to_member = true`
+    );
+    CIF.log("Generated new config file");
+};
+if (!fs.existsSync("./options.txt")) {
+    createNewFile();
 };
 
-export default configManager
+function stringToBoolean(str: string): boolean {
+    if (str === "true") return true;
+    else return false;
+}
 
+interface IConfiguration {
+    "bug": boolean
+    "combat": boolean
+    "crasher": boolean
+    "give": boolean
+    "instabreak": boolean
+    "join": boolean
+    "log": boolean
+    "movement": boolean
+    "scaffold": boolean
+    "xp": boolean
+    "ban": boolean
+    "kick": boolean
+    "send_to_member": boolean
+}
+type ConfigType = { [key in keyof IConfiguration]?: boolean };
+const config: ConfigType = {};
+try {
+    const optionFile = "../plugins/CIF/options.txt";
+    const options = fs.readFileSync(optionFile, "utf8");
+    const matcher = /^\s*([^=#]+)\s*=\s*(.*)\s*$/gm;
+    for (; ;) {
+        const matched = matcher.exec(options);
+        if (matched === null) break;
+        config[matched[1] as keyof IConfiguration] = stringToBoolean(matched[2]);
+    }
+} catch (err) {
+    throw err;
+}
 
-
-
-
-
-if (!existsSync("./options.txt")) {
-    configManager.createNewFile();
-};
+for (const [key, value] of Object.entries(config)) {
+    if (key === "ban" || key === "kick" || key === "send_to_member") {
+        CIFconfig.Penalties[key] = value;
+    } else {
+        CIFconfig.Modules[key as keyof IConfiguration] = value;
+    }
+}
