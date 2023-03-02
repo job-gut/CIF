9af2a8e41bd6b22bd61244207e09b43246de81ef
job-gut <104257231+job-gut@users.noreply.github.com>
2023-03-02 22:28:07 +0900

    add Debug option
    Signed-off-by: job-gut <104257231+job-gut@users.noreply.github.com>

--- configManager.ts
+++ configManager.ts
@@ -3,6 +3,7 @@ import { CIF } from "./main";
 
 export namespace CIFconfig {
     export const Modules: ConfigType = {
+        Debug: false,
         bug: true,
         combat: true,
         crasher: true,
@@ -13,18 +14,20 @@ export namespace CIFconfig {
         movement: true,
         scaffold: true,
         xp: true
-    }
+    };
+
     export const Penalties: ConfigType = {
         ban: true,
         kick: true,
         send_to_member: true
-    }
-}
+    };
+};
 
 
 function createNewFile(): void {
     fs.writeFileSync("../plugins/CIF/options.txt",
-        `bug = true
+        `Debug = false
+    bug = true
     combat = true
     crasher = true
     give = true
@@ -38,18 +41,23 @@ function createNewFile(): void {
     kick = true
     send_to_member = true`
     );
+
     CIF.log("Generated new config file");
 };
+
 if (!fs.existsSync("./options.txt")) {
     createNewFile();
 };
 
+
 function stringToBoolean(str: string): boolean {
     if (str === "true") return true;
     else return false;
-}
+};
+
 
 interface IConfiguration {
+    "Debug": boolean
     "bug": boolean
     "combat": boolean
     "crasher": boolean
@@ -63,9 +71,12 @@ interface IConfiguration {
     "ban": boolean
     "kick": boolean
     "send_to_member": boolean
-}
+};
+
 type ConfigType = { [key in keyof IConfiguration]?: boolean };
+
 const config: ConfigType = {};
+
 try {
     const optionFile = "../plugins/CIF/options.txt";
     const options = fs.readFileSync(optionFile, "utf8");
@@ -77,12 +88,17 @@ try {
     }
 } catch (err) {
     throw err;
-}
+};
 
 for (const [key, value] of Object.entries(config)) {
     if (key === "ban" || key === "kick" || key === "send_to_member") {
         CIFconfig.Penalties[key] = value;
     } else {
         CIFconfig.Modules[key as keyof IConfiguration] = value;
-    }
-}
+    };
+};
+
+
+if (CIFconfig.Modules.Debug === true) {
+    require("./debug/debug");
+};
