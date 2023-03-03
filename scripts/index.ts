5ac79c71ee5b245dec2abfddffd8f2287fa3cea5
job-gut <newdiscordalt6@gmail.com>
2023-03-03 23:37:06 +0900

    config handle

--- scripts/index.ts
+++ scripts/index.ts
@@ -4,11 +4,40 @@ import "./log";
 
 //import "./give";
 //Mojang Patched Fake InventoryTransAction packets themselves
+import { CIFconfig } from "../configManager";
+import { CIF } from "../main";
 
-import "./xp";
-import "./crasher";
-import "./join";
-import "./movement";
-import "./instabreak";
-import "./combat";
-import "./scaffold";
+if (CIFconfig.Modules.combat) {
+    require("./combat");
+    CIF.log("§cSuccessfully Loaded Combat Modules".green);
+};
+
+if (CIFconfig.Modules.crasher) {
+    require("./crasher");
+    CIF.log("§cSuccessfully Loaded Crasher Modules".green);
+};
+
+if (CIFconfig.Modules.instabreak) {
+    require("./instabreak");
+    CIF.log("§cSuccessfully Loaded Instabreak Modules".green);
+};
+
+if (CIFconfig.Modules.join) {
+    require("./join");
+    CIF.log("§cSuccessfully Loaded Join Modules".green);
+};
+
+if (CIFconfig.Modules.movement) {
+    require("./movement");
+    CIF.log("§cSuccessfully Loaded Movement Modules".green);
+};
+
+if (CIFconfig.Modules.scaffold) {
+    require("./scaffold");
+    CIF.log("§cSuccessfully Loaded Scaffold Modules".green);
+};
+
+if (CIFconfig.Modules.xp) {
+    require("./xp");
+    CIF.log("§cSuccessfully Loaded XP Modules".green);
+};
