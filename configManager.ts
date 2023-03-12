50751d4d5887dbe9061df6983ef1bb180256d6ba
wwwcomcomcomcom <iieiiergn@gmail.com>
2023-03-12 21:08:21 +0900

    server propertiees

--- configManager.ts
+++ configManager.ts
@@ -1,4 +1,13 @@
+import { Config } from "bdsx/config";
+import { serverProperties } from "bdsx/serverproperties";
+import * as path from "path";
 import * as fs from "fs";
+
+//  ██████ ██ ███████      ██████  ██████  ███    ██ ███████ ██  ██████
+// ██      ██ ██          ██      ██    ██ ████   ██ ██      ██ ██
+// ██      ██ █████       ██      ██    ██ ██ ██  ██ █████   ██ ██   ███
+// ██      ██ ██          ██      ██    ██ ██  ██ ██ ██      ██ ██    ██
+//  ██████ ██ ██           ██████  ██████  ██   ████ ██      ██  ██████
 
 export namespace CIFconfig {
     export const Modules: ConfigType = {
@@ -99,4 +108,57 @@ if (CIFconfig.Modules.Debug === true) {
     require("./debug/debug");
 };
 
+
+// ███████ ███████ ██████  ██    ██ ███████ ██████      ██████  ██████   ██████  ██████  ███████ ██████  ████████ ██ ███████ ███████
+// ██      ██      ██   ██ ██    ██ ██      ██   ██     ██   ██ ██   ██ ██    ██ ██   ██ ██      ██   ██    ██    ██ ██      ██
+// ███████ █████   ██████  ██    ██ █████   ██████      ██████  ██████  ██    ██ ██████  █████   ██████     ██    ██ █████   ███████
+//      ██ ██      ██   ██  ██  ██  ██      ██   ██     ██      ██   ██ ██    ██ ██      ██      ██   ██    ██    ██ ██           ██
+// ███████ ███████ ██   ██   ████   ███████ ██   ██     ██      ██   ██  ██████  ██      ███████ ██   ██    ██    ██ ███████ ███████
+if (serverProperties["server-authoritative-movement"] !== "client-auth") {
+    const propertyPath = Config.BDS_PATH + path.sep + "server.properties";
+    let properties = fs.readFileSync(propertyPath, "utf8");
+    const matcher = /^\s*([^=#]+)\s*=\s*(.*)\s*$/gm;
+
+
+    let hasbeenChanged = false;
+
+    while (true) {
+        const matched = matcher.exec(properties);
+        if (matched === null) break;
+        // console.log(matched[1], matched[2]);
+        const matchedIndex = properties.indexOf(`${matched[1]}=`);
+        // const matchedProperty = properties.substring(matchedIndex, matchedIndex + matched[1].length + matched[2].length + 1);
+        // console.log(matchedProperty);
+
+        const targetProperties: { [keyof: string]: string } = {
+            "player-movement-score-threshold": "0",
+            "player-movement-action-direction-threshold": "1",
+            "player-movement-distance-threshold": "0",
+            "player-movement-duration-threshold-in-ms": "0",
+            "correct-player-movement": "false",
+            "server-authoritative-block-breaking": "false",
+            "emit-server-telemetry": "true"
+        }
+
+        for (const [targetKey, targetValue] of Object.entries(targetProperties)) {
+            if (matched[1] === targetKey && matched[2] !== targetValue) {
+                properties = properties.replace(properties.substring(matchedIndex, matchedIndex + matched[1].length + matched[2].length + 1), `${targetKey}=${targetValue}`);
+                CIF.log(`${"Serverproperties".magenta} : Value of ${matched[1].cyan} changed from ${matched[2].green} to ${targetValue.green}`);
+                hasbeenChanged = true;
+                break;
+            }
+        }
+
+        // serverProperties[matched[1] as keyof ServerProperties] = matched[2];
+    }
+    if (hasbeenChanged) {
+        fs.writeFileSync(propertyPath, properties);
+        // console.log(properties);
+        CIF.log('ServerProperties Saved'.cyan);
+        CIF.log('Please restart the server.'.red);
+        setTimeout(() => {
+            process.exit(1);
+        }, 2000);
+    }
+}
 import "./scripts";
