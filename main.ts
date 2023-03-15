e36cbb4a5f8f10403b10f86632bec9e34ddba188
job-gut <newdiscordalt6@gmail.com>
2023-03-15 22:47:00 +0900

    client-auth 권장 삭제

--- main.ts
+++ main.ts
@@ -1,14 +1,9 @@
-import { serverProperties } from "bdsx/serverproperties";
 import { NetworkIdentifier } from "bdsx/bds/networkidentifier";
 import { CommandPermissionLevel } from "bdsx/bds/command";
 import { CANCEL } from "bdsx/common";
 
 function abstractFunction(): never {
     throw Error(`Failed to load "implements.ts"`);
-};
-
-if (serverProperties["server-authoritative-movement"] !== "client-auth") {
-    console.log("CIF는 client-auth 를 권장 합니다.".red);
 };
 
 export namespace CIF {
