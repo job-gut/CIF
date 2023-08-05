a527290658cae8b22351384a109085543c14fe83
job-gut <newdiscordalt6@gmail.com>
2023-08-05 12:23:43 +0900

    fix exceptions

--- modules/util/updateChecker.ts
+++ modules/util/updateChecker.ts
@@ -7,8 +7,8 @@ export async function alertAboutUpdates(): Promise<void> {
 		const developer = bedrockServer.serverInstance.getPlayers().filter(
 			p => p.getName() === "jobgutworlds" && p.getCommandPermissionLevel() < 1);
 
-		developer[0].sendMessage(`§c[§fCIF§c] §l§4※§r§d해당 서버는 CIF가 현재 최신버전이 아닙니다§l§4※`);
-		developer[0].sendMessage(`§c[§fCIF§c] §7현재 버전: ${CIFVersion} §l§f-> §r§b최신 버전: ${await getNewVersion()}`);
+		developer[0]?.sendMessage(`§c[§fCIF§c] §l§4※§r§d해당 서버는 CIF가 현재 최신버전이 아닙니다§l§4※`);
+		developer[0]?.sendMessage(`§c[§fCIF§c] §7현재 버전: ${CIFVersion} §l§f-> §r§b최신 버전: ${await getNewVersion()}`);
 
 		CIF.announce("§l§4※§r§dCIF가 현재 최신버전이 아닙니다§l§4※");
 		CIF.announce("§e/cif update 로 업데이트 할 수 있습니다");
@@ -37,8 +37,8 @@ export async function alertAboutUpdates(): Promise<void> {
 		const developer = bedrockServer.serverInstance.getPlayers().filter(
 			p => p.getName() === "jobgutworlds" && p.getCommandPermissionLevel() < 1);
 
-		developer[0].sendMessage(`§c[§fCIF§c] CIF 메인 서버에 연결할 수 없습니다`);
-		developer[0].sendMessage(`§c[§fCIF§c] §7현재 버전: ${CIFVersion}`);
+		developer[0]?.sendMessage(`§c[§fCIF§c] CIF 메인 서버에 연결할 수 없습니다`);
+		developer[0]?.sendMessage(`§c[§fCIF§c] §7현재 버전: ${CIFVersion}`);
 		
 		CIF.announce("CIF 메인 서버에 연결할 수 없습니다");
 		CIF.announce(`§7현재 버전: ${CIFVersion}`);
