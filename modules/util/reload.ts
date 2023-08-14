84bfd7d6e39a61184e35e915ccaf54025f3184a1
job-gut <newdiscordalt6@gmail.com>
2023-08-14 20:21:56 +0900

    fixed Reload Bug FINALLY

--- modules/util/reload.ts
+++ modules/util/reload.ts
@@ -1,13 +1,37 @@
+import { CommandPermissionLevel } from "bdsx/bds/command";
+import { command } from "bdsx/command";
 import { events } from "bdsx/event";
 import { exec } from "child_process";
+import { CIF } from "../../main";
+import { bedrockServer } from "bdsx/launcher";
+import { CANCEL } from "bdsx/common";
+
+const filePath = "../plugins/cif/modules/scripts/reloadedScript.ts";
+
+let isLoading = false;
 
 events.packetBefore(77).on((pkt, ni) => {
-    const pl = ni.getActor()!;
-    if (pl.getCommandPermissionLevel() > 0)
-        if (pkt.command === "/reload") {
-			exec("tsc --strict ../plugins/cif/modules/scripts/reloadedScript.ts", ((err)=> {
+	const pl = ni.getActor()!;
+	if (pl.getCommandPermissionLevel() > 0) {
+		if (pkt.command === "/reload") {
+			if (isLoading === true) {
+				pl.sendMessage("§c현재 개발 스크립트가 로딩중에 있습니다");
+				pl.sendMessage("§c잠시 후 다시 시도해주세요");
+				return CANCEL;
+			};
+
+			isLoading = true;
+
+			exec(`cmd /c tsc --strict ${filePath}`, ((err) => {
 				if (err) throw err;
 				require("../scripts/reloadedScript");
+				CIF.announce(`§l${pl.getName()} §e > §dReloaded Development Script`);
+				isLoading = false;
 			}));
-    	};
+		};
+	};
+});
+
+bedrockServer.afterOpen().then(() => {
+	command.find("reload").signature.permissionLevel = CommandPermissionLevel.Operator;
 });
